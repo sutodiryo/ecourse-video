c2faaf428ccffb26162db876302a659be46d7f32
sutodiryo <hubungi.yogi@gmail.com>
2024-06-05 08:25:54 +0700

    -

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,6 +6,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -17,9 +20,12 @@ class UserController extends Controller
     public function index()
     {
         // get all users with roles, search and paginate
-        $users = User::whereNot('email','admin@gmail.com')->with('roles')->when(request()->q, function($search){
-            $search = $search->where('name', 'like', '%'.request()->q.'%');
-        })->paginate(10);
+        $users = User::with('roles')
+            // ->whereNot('email', 'admin@gmail.com')
+            ->when(request()->q, function ($search) {
+                $search = $search->where('name', 'like', '%' . request()->q . '%');
+            })
+            ->paginate(10);
 
         // get all roles
         $roles = Role::get();
@@ -40,10 +46,24 @@ class UserController extends Controller
         // update roles for user
         // $user->syncRoles($request->role);
 
+        DB::beginTransaction();
         $data = $request->except('role');
-        $user->update($data);
 
+        if ($user->uu_id) {
+            $user->update($data);
+        } else {
+            $data['uu_id'] = Str::orderedUuid();
+            $user->update($data);
+        }
+
+        if ($request->reset_password) {
+            $data['password'] = Hash::make(123123123);
+            $user->update($data);
+        }
+        // dd($data);
+        // dd('stop');
+        DB::commit();
         // return back with toastr
-        return back()->with('toast_success', 'User roles updated successfully');
+        return back()->with('toast_success', 'User data updated successfully');
     }
 }
